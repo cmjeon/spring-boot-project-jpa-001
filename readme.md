ced5c6aa9914f033752a5cb2beea685c7774975b
cmj <chmin82@gmail.com>
2022-01-29 20:38:56 +0900

    1.4 어플리케이션 실행하기

---
+++ readme.md
@@ -0,0 +1,5 @@
+## 개요
+
+백견불여일타 이젠 프로젝트다! 스프링 부트 쇼핑몰 프로젝트 with JPA 따라하기
+
+http://www.yes24.com/product/goods/103453774
---
+++ src/main/java/com/example/SpringDemoApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+	@GetMapping(value = "/")
+	public String HelloWorld() {
+		return "Hello World";
+	}
+}
