c02d192a84e476a1ed8732f39a1afeb2e40d6b7e
cmj <chmin82@gmail.com>
2022-03-01 15:59:29 +0900

    4.5 페이지 권한 설정하기

--- src/main/java/com/shop/demoShop/config/SecurityConfig.java
+++ src/main/java/com/shop/demoShop/config/SecurityConfig.java
@@ -8,16 +8,38 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.shop.demoShop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  MemberService memberService;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.formLogin()
+        .loginPage("/members/login") // 1
+        .defaultSuccessUrl("/") // 2
+        .usernameParameter("email") // 3
+        .failureUrl("/members/login/error") // 4
+        .and()
+        .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) // 5
+        .logoutSuccessUrl("/"); // 6
   }
 
   @Bean
-  public PasswordEncoder passwordEncoder() {
+  public PasswordEncoder passwordEncoder(){
     return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception { // 7
+    auth.userDetailsService(memberService).passwordEncoder(passwordEncoder()); // 8
   }
 }
---
+++ src/test/java/com/shop/demoShop/controller/MemberControllerTest.java
@@ -0,0 +1,69 @@
+package com.shop.demoShop.controller;
+
+import com.shop.demoShop.dto.MemberFormDto;
+import com.shop.demoShop.entity.Member;
+import com.shop.demoShop.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc // 1
+@Transactional
+@TestPropertySource(locations="classpath:application-test.properties")
+class MemberControllerTest {
+
+  @Autowired
+  private MemberService memberService;
+
+  @Autowired
+  private MockMvc mockMvc; // 2
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  public Member createMember(String email, String password) { // 3
+    MemberFormDto memberFormDto = new MemberFormDto();
+    memberFormDto.setEmail(email);
+    memberFormDto.setName("홍길동");
+    memberFormDto.setAddress("서울시 마포구 합정동");
+    memberFormDto.setPassword(password);
+    Member member = Member.createMember(memberFormDto, passwordEncoder);
+    return memberService.saveMember(member);
+  }
+
+  @Test
+  @DisplayName("로그인 성공 테스트")
+  public void loginSuccessTest() throws Exception {
+    String email = "test@email.com";
+    String password = "1234";
+    this.createMember(email, password);
+    mockMvc.perform(formLogin()
+      .userParameter("email")
+      .loginProcessingUrl("/members/login") // 4
+      .user(email).password(password))
+      .andExpect(SecurityMockMvcResultMatchers.authenticated()); // 5
+  }
+
+  @Test
+  @DisplayName("로그인 실패 테스트")
+  public void loginFailTest() throws Exception {
+    String email = "test@email.com";
+    String password = "1234";
+    this.createMember(email, password);
+    mockMvc.perform(formLogin().userParameter("email")
+      .loginProcessingUrl("/members/login")
+      .user(email)
+      .password("12345"))
+      .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+  }
+}
