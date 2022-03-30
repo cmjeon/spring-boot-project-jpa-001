78a7444b87227ec54ce60dcfa9827f634530ddbe
cmj <chmin82@gmail.com>
2022-03-30 19:43:47 +0900

    7장 주문

--- src/main/java/com/shop/demoShop/controller/ItemController.java
+++ src/main/java/com/shop/demoShop/controller/ItemController.java
@@ -105,4 +105,11 @@ public class ItemController {
     return "item/itemMng";
   }
 
+  @GetMapping(value = "/item/{itemId}")
+  public String itemDtl(Model model, @PathVariable("itemId") Long itemId){
+    ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+    model.addAttribute("item", itemFormDto);
+    return "item/itemDtl";
+  }
+
 }
